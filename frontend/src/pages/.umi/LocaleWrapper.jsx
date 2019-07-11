2af6180eb0c5824e672b333520b3c9b8de7bb43b
felix <felixglow@163.com>
2019-07-11 14:46:41 +0800

    add package.json

--- frontend/src/pages/.umi/LocaleWrapper.jsx
+++ frontend/src/pages/.umi/LocaleWrapper.jsx
@@ -1,18 +1,4 @@
-
-import { _setIntlObject, addLocaleData, IntlProvider, intlShape } from 'umi-plugin-locale';
-
-const InjectedWrapper = (() => {
-  let sfc = (props, context) => {
-    _setIntlObject(context.intl);
-    return props.children;
-  };
-  sfc.contextTypes = {
-    intl: intlShape,
-  };
-  return sfc;
-})();
-
-import 'moment/locale/zh-cn';
+import React from 'react';
 
 const baseNavigator = true;
 const useLocalStorage = true;
@@ -23,50 +9,67 @@ import 'moment/locale/zh-cn';
 let defaultAntd = require('antd/lib/locale-provider/zh_CN');
 defaultAntd = defaultAntd.default || defaultAntd;
 
-const localeInfo = {
-  'en-US': {
-    messages: {
-      ...require('/Users/felix/giant/stark/frontend/src/locales/en-US.js').default,
-    },
-    locale: 'en-US',
-    antd: require('antd/lib/locale-provider/en_US'),
-    data: require('react-intl/locale-data/en'),
-    momentLocale: '',
-  },
-  'zh-CN': {
-    messages: {
-      ...require('/Users/felix/giant/stark/frontend/src/locales/zh-CN.js').default,
-    },
+const localeInfo = {};
+
+class LocaleWrapper extends React.Component {
+  state = {
     locale: 'zh-CN',
-    antd: require('antd/lib/locale-provider/zh_CN'),
-    data: require('react-intl/locale-data/zh'),
-    momentLocale: 'zh-cn',
-  },
-};
+  };
+  getAppLocale() {
+    let appLocale = {
+      locale: 'zh-CN',
+      messages: {},
+      data: require('react-intl/locale-data/zh'),
+      momentLocale: 'zh-cn',
+    };
 
-let appLocale = {
-  locale: 'zh-CN',
-  messages: {},
-  data: require('react-intl/locale-data/zh'),
-  momentLocale: 'zh-cn',
-};
-if (useLocalStorage && localStorage.getItem('umi_locale') && localeInfo[localStorage.getItem('umi_locale')]) {
-  appLocale = localeInfo[localStorage.getItem('umi_locale')];
-} else if (localeInfo[navigator.language] && baseNavigator){
-  appLocale = localeInfo[navigator.language];
-} else {
-  appLocale = localeInfo['zh-CN'] || appLocale;
-}
-window.g_lang = appLocale.locale;
-appLocale.data && addLocaleData(appLocale.data);
+    const runtimeLocale = require('umi/_runtimePlugin').mergeConfig('locale') || {};
+    const runtimeLocaleDefault =
+      typeof runtimeLocale.default === 'function' ? runtimeLocale.default() : runtimeLocale.default;
+    if (
+      useLocalStorage &&
+      typeof localStorage !== 'undefined' &&
+      localStorage.getItem('umi_locale') &&
+      localeInfo[localStorage.getItem('umi_locale')]
+    ) {
+      appLocale = localeInfo[localStorage.getItem('umi_locale')];
+    } else if (
+      typeof navigator !== 'undefined' &&
+      localeInfo[navigator.language] &&
+      baseNavigator
+    ) {
+      appLocale = localeInfo[navigator.language];
+    } else if (localeInfo[runtimeLocaleDefault]) {
+      appLocale = localeInfo[runtimeLocaleDefault];
+    } else {
+      appLocale = localeInfo['zh-CN'] || appLocale;
+    }
+    window.g_lang = appLocale.locale;
 
-export default function LocaleWrapper(props) {
-  let ret = props.children;
-  ret = (<IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
-    <InjectedWrapper>{ret}</InjectedWrapper>
-  </IntlProvider>)
-  ret = (<LocaleProvider locale={appLocale.antd ? (appLocale.antd.default || appLocale.antd) : defaultAntd}>
-    {ret}
-  </LocaleProvider>);
-  return ret;
+    return appLocale;
+  }
+  reloadAppLocale = () => {
+    const appLocale = this.getAppLocale();
+    this.setState({
+      locale: appLocale.locale,
+    });
+  };
+
+  render() {
+    const appLocale = this.getAppLocale();
+    const LangContextValue = {
+      locale: appLocale.locale,
+      reloadAppLocale: this.reloadAppLocale,
+    };
+    let ret = this.props.children;
+    return (
+      <LocaleProvider
+        locale={appLocale.antd ? appLocale.antd.default || appLocale.antd : defaultAntd}
+      >
+        {ret}
+      </LocaleProvider>
+    );
+    return ret;
+  }
 }
+export default LocaleWrapper;
--- frontend/src/pages/.umi/router.js
+++ frontend/src/pages/.umi/router.js
@@ -1,56 +1,76 @@
 import React from 'react';
 import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
 import dynamic from 'umi/dynamic';
-import renderRoutes from 'umi/_renderRoutes';
-import RendererWrapper0 from '/Users/felix/giant/stark/frontend/src/pages/.umi/LocaleWrapper.jsx'
-import _dvaDynamic from 'dva/dynamic'
+import renderRoutes from 'umi/lib/renderRoutes';
+import history from '@tmp/history';
+import RendererWrapper0 from '/Users/felix/3myproject/stark/frontend/src/pages/.umi/LocaleWrapper.jsx';
+import _dvaDynamic from 'dva/dynamic';
 
-let Router = require('dva/router').routerRedux.ConnectedRouter;
+const Router = require('dva/router').routerRedux.ConnectedRouter;
 
-let routes = [
+const routes = [
   {
-    "path": "/",
-    "component": _dvaDynamic({
-  
-  component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../../layouts/BasicLayout'),
-  LoadingComponent: require('/Users/felix/giant/stark/frontend/src/components/PageLoading/index').default,
-}),
-    "Routes": [require('../Authorized').default],
-    "routes": [
+    path: '/',
+    component: __IS_BROWSER
+      ? _dvaDynamic({
+          component: () =>
+            import(/* webpackChunkName: "layouts__BasicLayout" */ '../../layouts/BasicLayout'),
+          LoadingComponent: require('/Users/felix/3myproject/stark/frontend/src/components/PageLoading/index')
+            .default,
+        })
+      : require('../../layouts/BasicLayout').default,
+    Routes: [require('../Authorized').default],
+    routes: [
       {
-        "path": "/",
-        "name": "dashboard",
-        "hideInMenu": true,
-        "component": _dvaDynamic({
-  
-  component: () => import(/* webpackChunkName: "p__Dashboard__HomePage" */'../Dashboard/HomePage'),
-  LoadingComponent: require('/Users/felix/giant/stark/frontend/src/components/PageLoading/index').default,
-}),
-        "exact": true
+        path: '/',
+        name: 'dashboard',
+        hideInMenu: true,
+        component: __IS_BROWSER
+          ? _dvaDynamic({
+              component: () =>
+                import(/* webpackChunkName: "p__Dashboard__HomePage" */ '../Dashboard/HomePage'),
+              LoadingComponent: require('/Users/felix/3myproject/stark/frontend/src/components/PageLoading/index')
+                .default,
+            })
+          : require('../Dashboard/HomePage').default,
+        exact: true,
       },
       {
-        "component": _dvaDynamic({
-  
-  component: () => import(/* webpackChunkName: "p__404" */'../404'),
-  LoadingComponent: require('/Users/felix/giant/stark/frontend/src/components/PageLoading/index').default,
-}),
-        "exact": true
+        component: __IS_BROWSER
+          ? _dvaDynamic({
+              component: () => import(/* webpackChunkName: "p__404" */ '../404'),
+              LoadingComponent: require('/Users/felix/3myproject/stark/frontend/src/components/PageLoading/index')
+                .default,
+            })
+          : require('../404').default,
+        exact: true,
       },
       {
-        "component": () => React.createElement(require('/Users/felix/giant/stark/frontend/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
-      }
-    ]
+        component: () =>
+          React.createElement(
+            require('/Users/felix/3myproject/stark/frontend/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+              .default,
+            { pagesPath: 'src/pages', hasRoutesInConfig: true }
+          ),
+      },
+    ],
   },
   {
-    "component": () => React.createElement(require('/Users/felix/giant/stark/frontend/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
-  }
+    component: () =>
+      React.createElement(
+        require('/Users/felix/3myproject/stark/frontend/node_modules/umi-build-dev/lib/plugins/404/NotFound.js')
+          .default,
+        { pagesPath: 'src/pages', hasRoutesInConfig: true }
+      ),
+  },
 ];
 window.g_routes = routes;
-window.g_plugins.applyForEach('patchRoutes', { initialValue: routes });
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
 
 // route change handler
 function routeChangeHandler(location, action) {
-  window.g_plugins.applyForEach('onRouteChange', {
+  plugins.applyForEach('onRouteChange', {
     initialValue: {
       routes,
       location,
@@ -58,15 +78,15 @@ function routeChangeHandler(location, action) {
     },
   });
 }
-window.g_history.listen(routeChangeHandler);
-routeChangeHandler(window.g_history.location);
+history.listen(routeChangeHandler);
+routeChangeHandler(history.location);
 
-export default function RouterWrapper() {
+export { routes };
+
+export default function RouterWrapper(props = {}) {
   return (
-<RendererWrapper0>
-          <Router history={window.g_history}>
-      { renderRoutes(routes, {}) }
-    </Router>
-        </RendererWrapper0>
+    <RendererWrapper0>
+      <Router history={history}>{renderRoutes(routes, props)}</Router>
+    </RendererWrapper0>
   );
 }
